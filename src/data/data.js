0204b89281b74b2be01f1256f7c1c7c804cb148f
kouahodavid6 <kouahodavid6@gmail.com>
2025-11-23 11:06:37 +0000

    Numéro whatsapp

--- src/data/data.js
+++ src/data/data.js
@@ -5,7 +5,7 @@ const developpeurs = [
         bio: "Passionné par la création d'expériences mobiles fluides avec React Native. Transforme les exigences complexes en applications intuitives et performantes.",
         technologies: ["React Native", "Expo", "REST API"],
         linkedin: "#",
-        whatsapp: "",
+        whatsapp: "https://wa.me/+2250564624366",
         image: "download (1).jpeg"
     },
     {
@@ -14,7 +14,7 @@ const developpeurs = [
         bio: "Expert dans la construction d'architectures serveur robustes et évolutives. Spécialisé dans la conception d'API et l'optimisation des bases de données.",
         technologies: ["Laravel", "MySQL", "RESTful APIs"],
         linkedin: "#",
-        whatsapp: "#",
+        whatsapp: "https://wa.me/+2250140022693",
         image: "download (2).jpeg"
     },
     {
@@ -23,7 +23,7 @@ const developpeurs = [
         bio: "Crée des interfaces web magnifiques et réactives avec les frameworks modernes. Axé sur l'expérience utilisateur et les implémentations pixel-perfect.",
         technologies: ["React.js", "Tailwind CSS", "JavaScript"],
         linkedin: "https://www.linkedin.com/in/david-kouaho?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-        whatsapp: "https://wa.me/0171136261",
+        whatsapp: "https://wa.me/+2250171136261",
         image: "download (3).jpeg"
     }
 ];
