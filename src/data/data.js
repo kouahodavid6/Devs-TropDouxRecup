f2c933e6e9f21a751f4c721e9b4513b7f2883078
kouahodavid6 <kouahodavid6@gmail.com>
2025-11-23 11:16:12 +0000

    data.js

--- src/data/data.js
+++ src/data/data.js
@@ -2,8 +2,8 @@ const developpeurs = [
     {
         name: "Judicael Cakpo",
         role: "Développeur Front-End Mobile",
-        bio: "Passionné par la création d'expériences mobiles fluides avec React Native. Transforme les exigences complexes en applications intuitives et performantes.",
-        technologies: ["React Native", "Tailwind CSS", "Expo", "REST API"],
+        bio: "Passionné par la création d'expériences mobiles fluides avec React Native.",
+        technologies: ["React Native", "Tailwind CSS", "REST API"],
         linkedin: "#",
         whatsapp: "https://wa.me/+2250564624366",
         image: "download (1).jpeg"
@@ -11,7 +11,7 @@ const developpeurs = [
     {
         name: "Emmanuel Bamidélé",
         role: "Développeur Back-End",
-        bio: "Expert dans la construction d'architectures serveur robustes et évolutives. Spécialisé dans la conception d'API et l'optimisation des bases de données.",
+        bio: "Spécialisé dans la conception d'API et l'optimisation des bases de données.",
         technologies: ["Laravel", "MySQL", "RESTful APIs"],
         linkedin: "#",
         whatsapp: "https://wa.me/+2250140022693",
@@ -20,7 +20,7 @@ const developpeurs = [
     {
         name: "David Kouaho",
         role: "Développeur Front-End Web",
-        bio: "Crée des interfaces web magnifiques et réactives avec les frameworks modernes. Axé sur l'expérience utilisateur et les implémentations pixel-perfect.",
+        bio: "Crée des interfaces web magnifiques et réactives avec les frameworks modernes.",
         technologies: ["React.js", "Tailwind CSS", "REST API"],
         linkedin: "https://www.linkedin.com/in/david-kouaho?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
         whatsapp: "https://wa.me/+2250171136261",
